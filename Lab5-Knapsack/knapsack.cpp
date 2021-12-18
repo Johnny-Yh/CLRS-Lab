709266eeb01e120a71fc5c76836d1c0d133b59a1
Johnny-Yh <1351650904@qq.com>
2021-12-18 14:55:00 +0800

    update

--- Lab5-Knapsack/knapsack.cpp
+++ Lab5-Knapsack/knapsack.cpp
@@ -2,46 +2,50 @@
 using namespace std;
 
 int dynamic(vector<int> &weight,vector<int> &value,int c){
-    // 动态规划方法
+    // 动态规划方法解决背包问题
     int n=weight.size();
-    vector<vector<int>> dp(c+1,vector<int>(n+1));
-    // dp[i][j]->背包容量为j 前i个物品能达到的最大价值
-    for(int i=1;i<=c;++i){
-        for(int j=1;j<=n;++j){
-            // 装不下 和原来的最大价值一样
-            if(weight[i-1]>j) dp[i][j]=dp[i-1][j];
-            else{
-                // 装得下 判断是否装入
-                dp[i][j]=max(dp[i-1][j-weight[i-1]]+value[i-1],dp[i-1][j]);
+    // dp[i][j]->前i件物品装入限重为j的背包能获得的最大价值
+    vector<vector<int>> dp(n+1,vector<int>(c+1));
+    for(int i=1;i<=n;++i){
+        for(int j=1;j<=c;++j){
+            if(j>=weight[i-1]){
+                // 可以装的下第i件物品
+                dp[i][j]=max(dp[i-1][j],dp[i-1][j-weight[i-1]]+value[i-1]);
             }
+            // 装不下第i件物品
+            else dp[i][j]=dp[i-1][j];
         }
     }
-    return dp[c][n];
+    return dp[n][c];
 }
 
 int greedy(vector<int> &weight,vector<int> &value,int c){
+    // 贪心方法解决背包问题
     int n=weight.size(),res=0;
     vector<pair<int,int>> vpw(n);
-    for(i=0;i<n;++j) {
+    for(int i=0;i<n;++i) {
         vpw[i].first=weight[i];// 重量
         vpw[i].second=value[i];// 价值
     }
+    // 按单位价值降序排序
     auto cmp=[&](pair<int,int> x,pair<int,int> y){
-        return double(x.second/x.first)>double(y.second/y.first);
+        return (double)x.second/(double)x.first>
+        (double)y.second/(double)y.first;
     };
-    sort(vpw.begin(),vpw.end(),cmp);// 按单位价值降序排序
-    for(int i=0;i<n&&c>0;++i){
+    sort(vpw.begin(),vpw.end(),cmp);
+    for(int i=0;i<n;++i){
+        if(c<vpw[i].first) break;
         // 从大到小装入
         res+=vpw[i].second;
-        c-=vpw[i].first;
+        c-=vpw[i].first;  
     }
     return res;
 }
 int main(){
-    vector<int> weight={2,2,6,5,4};
-    vector<int> value={6,3,5,4,6};
+    vector<int> weight={2,2,6,5,4};// 重量
+    vector<int> value={6,3,5,4,6};// 价值
     int c=10;// 背包容量
-    cout<<dynamic(weight,value)<<endl;
-    cout<<greedy(weight,value)<<endl;
+    cout<<"动态规划算出的最大价值:"<<dynamic(weight,value,c)<<endl;
+    cout<<"贪心算出的最大价值:"<<greedy(weight,value,c)<<endl;
     return 0;
 }
